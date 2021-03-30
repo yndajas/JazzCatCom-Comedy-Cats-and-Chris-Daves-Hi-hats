cdae511d9013aba846b3c9254e21586cc4128bca
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-03-30 21:43:19 +0100

    update schema

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_032423) do
+ActiveRecord::Schema.define(version: 2021_03_30_194929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,4 +29,23 @@ ActiveRecord::Schema.define(version: 2021_03_30_032423) do
     t.index ["artist_id"], name: "index_jazz_videos_on_artist_id"
   end
 
+  create_table "user_jazz_videos", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "jazz_video_id"
+    t.boolean "approved"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["jazz_video_id"], name: "index_user_jazz_videos_on_jazz_video_id"
+    t.index ["user_id"], name: "index_user_jazz_videos_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "user_jazz_videos", "jazz_videos"
+  add_foreign_key "user_jazz_videos", "users"
 end
