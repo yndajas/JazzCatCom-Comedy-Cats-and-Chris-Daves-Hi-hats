3536bc82686d900c8f909bb6cc0d850117d029f2
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-04-01 11:55:14 +0100

    create register route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   post 'sessions' => 'sessions#create'
   delete 'sessions' => 'sessions#destroy'
-  resources :users
+  post 'users' => 'users#create'
+  # resources :users
   get '/jazz-videos' => 'jazz_videos#index'
   get '/users/:id/jazz-videos' => 'jazz_videos#index'
   get '/users/:id/jazz-videos/unseen' => 'jazz_videos#index'
