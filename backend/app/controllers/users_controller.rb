86046f029010acd1e850051d39517a915a004094
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-04-02 22:35:28 +0100

    flesh out users#create action (error handling to follow)

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
     def create
-        binding.pry
+        if User.find_by(email: params[:email])
+            # error - user exists
+        else
+            user = User.create(email: params[:email], password: params[:password])
+            session[:user_id] = user.id
+            render json: {user: user.email}
+        end        
     end
 end
