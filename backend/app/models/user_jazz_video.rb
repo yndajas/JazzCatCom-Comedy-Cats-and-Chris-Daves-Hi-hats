ea73006d398aa55a8a9a82c95119ea37724866ed
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-04-08 13:02:14 +0100

    add approve or reject method for saving user jazz videos

--- backend/app/models/user_jazz_video.rb
+++ backend/app/models/user_jazz_video.rb
@@ -1,4 +1,10 @@
 class UserJazzVideo < ApplicationRecord
   belongs_to :user
   belongs_to :jazz_video
+
+  def self.approve_or_reject(user_id, jazz_video_id, approve_or_reject)
+    approved = (approve_or_reject == 'approve') ? true : false
+    user_jazz_video = UserJazzVideo.find_or_create_by(user_id: user_id, jazz_video_id: jazz_video_id)
+    user_jazz_video.update(approved: approved)
+  end
 end
