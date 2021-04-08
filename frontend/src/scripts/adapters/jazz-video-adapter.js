a44589e73940651bf5913bfb0c420739ce0e664f
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-04-08 13:01:48 +0100

    add video saving API POST call

--- frontend/src/scripts/adapters/jazz-video-adapter.js
+++ frontend/src/scripts/adapters/jazz-video-adapter.js
@@ -7,4 +7,17 @@ class JazzVideoAdapter {
         let response = await fetch(`${JazzVideoAdapter.backendBaseUrl}users/${userId}/jazz-videos/unseen`)
         return await response;
     }
+
+    static async saveVideo(userId, vid, approveOrReject) {
+        let response = await fetch(`${JazzVideoAdapter.backendBaseUrl}users/${userId}/jazz-videos`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },        
+            body: JSON.stringify({vid, approveOrReject})
+        })
+        return await response;
+        // console.log(`${userId}, ${vid}, ${approveOrReject}`);
+    }
 }
