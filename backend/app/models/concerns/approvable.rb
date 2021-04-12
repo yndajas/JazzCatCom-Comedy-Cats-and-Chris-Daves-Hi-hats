58f526441a274932a5d607a8edd545bbd5900e00
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-04-12 19:44:48 +0100

    refactor to work with external resources, instantiating as needed

--- backend/app/models/concerns/approvable.rb
+++ backend/app/models/concerns/approvable.rb
@@ -1,8 +1,10 @@
 module Approvable
     module ClassMethods
-        def approve_or_reject(user_id, instance_id, approve_or_reject)
+        def approve_or_reject(user_id, identifier, approve_or_reject)
             approved = (approve_or_reject == 'approve') ? true : false
-            user_resource_instance = self.find_or_create_by(user_id: user_id, :"#{self.name.gsub("User", "").underscore}_id" => instance_id)
+            resource_model = self.name.gsub("User", "").constantize
+            resource_instance = resource_model.find_or_create_by(:"#{resource_model.identifier}" => identifier)
+            user_resource_instance = self.find_or_create_by(user_id: user_id, :"#{self.name.gsub("User", "").underscore}_id" => resource_instance.id)
             user_resource_instance.update(approved: approved)
         end
     end
