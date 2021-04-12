8e0ed16d9359c87a8916207eb05c0a2bc3557449
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-04-12 17:46:07 +0100

    refactor approved/unseen methods to work with multiple models

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,17 +1,21 @@
 class User < ApplicationRecord
     has_many :user_jazz_videos
+    has_many :user_cats
     has_many :jazz_videos, through: :user_jazz_videos
+    has_many :cats, through: :user_cats
     has_secure_password
 
-    def approved_jazz_videos
-        self.jazz_videos.map do |jazz_video|
-            jazz_video if self.user_jazz_videos.find_by(jazz_video_id: jazz_video.id).approved
+    def approved(resource)
+        self.send(resource).map do |instance|
+            instance if self.send("user_#{resource}").find_by(:"#{resource.singularize}_id" => instance.id).approved
         end.compact
     end
 
-    def unseen_jazz_videos
-        JazzVideo.all.map do |jazz_video|
-            jazz_video if !self.user_jazz_videos.find_by(jazz_video_id: jazz_video.id)
+
+    def unseen(resource)
+        cclass = resource.classify.constantize
+        cclass.all.map do |instance|
+            instance if !self.send("user_#{resource}").find_by(:"#{resource.singularize}_id" => instance.id)
         end.compact
     end
 end
