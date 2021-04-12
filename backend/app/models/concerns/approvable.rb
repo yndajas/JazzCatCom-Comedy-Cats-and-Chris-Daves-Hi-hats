791ab199df246c39205a5a6a1e94aab3eef1b64d
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-04-13 00:15:01 +0100

    deal with setting additional attributes in Approvable

--- backend/app/models/concerns/approvable.rb
+++ backend/app/models/concerns/approvable.rb
@@ -1,9 +1,19 @@
 module Approvable
     module ClassMethods
-        def approve_or_reject(user_id, identifier, approve_or_reject)
+        def approve_or_reject(user_id, identifier, approve_or_reject, additional_attributes = null)
             approved = (approve_or_reject == 'approve') ? true : false
             resource_model = self.name.gsub("User", "").constantize
-            resource_instance = resource_model.find_or_create_by(:"#{resource_model.identifier}" => identifier)
+            resource_instance = resource_model.find_or_initialize_by(:"#{resource_model.identifier}" => identifier)
+            
+            if resource_instance.new_record?
+                if additional_attributes
+                    additional_attributes.each do |key, value|
+                        resource_instance.send("#{key}=", value)
+                    end
+                end
+                resource_instance.save
+            end
+
             user_resource_instance = self.find_or_create_by(user_id: user_id, :"#{self.name.gsub("User", "").underscore}_id" => resource_instance.id)
             user_resource_instance.update(approved: approved)
         end
