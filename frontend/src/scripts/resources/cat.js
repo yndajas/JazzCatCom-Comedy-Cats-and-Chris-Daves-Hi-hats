d2f2864996edf7e7b6a18d153138bac4f4d32526
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-04-12 19:41:48 +0100

    add Cat JavaScript class based on JazzVideo template

---
+++ frontend/src/scripts/resources/cat.js
@@ -0,0 +1,65 @@
+class Cat {
+    constructor(filename) {
+        this.filename = filename;
+    }
+
+    htmlElements(app, forApproval = true) {
+        const div = document.createElement('div');
+        div.className = 'resource-container';
+
+        const img = document.createElement('img');
+        img.setAttribute('src', this.url);
+
+        div.append(img);
+
+        if (forApproval) {
+            return this.htmlElementsForApproval(div, app);
+        } else {
+            return div;
+            // might want to add a delete button here (and refactor into a deparate method if so)
+        }
+    }
+
+    htmlElementsForApproval(div, app) {
+        const approveButton = document.createElement('button');
+        approveButton.id = 'approve'
+        approveButton.className = 'btn btn-success';
+        approveButton.innerText = "Meow";
+        approveButton.addEventListener('click', () => {
+            CatAdapter.save(app.userId, this.filename, 'approve')
+            .then(() => app.renderRandomCat())
+        })
+
+        const rejectButton = document.createElement('button');
+        rejectButton.id = 'reject'
+        rejectButton.className = 'btn btn-danger';
+        rejectButton.innerText = "Woof";
+        rejectButton.addEventListener('click', () => {
+            CatAdapter.save(app.userId, this.filename, 'reject')
+            .then(() => app.renderRandomCat())
+        })
+
+        const br = document.createElement('br');
+        const spaces = App.createSpaces(1);
+
+        return [div, br, approveButton, spaces[0], rejectButton];
+    }
+
+    get url() {
+        return `https://purr.objects-us-east-1.dream.io/i/${this.filename}`
+    }
+
+    static allApprovedFromJson(json) {
+        const count = json.data.length;
+        if (count === 0) {
+            return "No approved cats";
+        } else {
+            return json.data.map(catData => new Cat(catData.attributes.filename))
+        }
+    }
+
+    static fromJson(json) {
+        const filename = json.file.replace("https://purr.objects-us-east-1.dream.io/i/", "");
+        return new Cat(filename);
+    }
+}
