39d42ede2eae47bb5a6255ead80cc68d85f17c65
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-04-12 23:57:00 +0100

    add joke JavaScript class

---
+++ frontend/src/scripts/resources/joke.js
@@ -0,0 +1,67 @@
+class Joke {
+    constructor(external_api_id, category, setup, punchline) {
+        this.external_api_id = external_api_id;
+        this.category = category;
+        this.setup = setup;
+        this.punchline = punchline; 
+    }
+
+    static get randomUrl() {
+        return "https://official-joke-api.appspot.com/jokes/random";
+    }
+
+    static get resource() {
+        return "jokes";
+    }
+
+    get additional_attributes() {
+        return {category: this.category, setup: this.setup, punchline: this.punchline};
+    }
+
+    htmlElements(app, forApproval = true) {
+        const div = document.createElement('div');
+        div.className = 'resource-container';
+
+        const p = document.createElement('p');
+        p.innerText = `${this.category} ${this.setup} ${this.punchline}`;
+
+        div.append(p);
+
+        if (forApproval) {
+            return this.htmlElementsForApproval(div, app);
+        } else {
+            return div;
+            // might want to add a delete button here (and refactor into a deparate method if so)
+        }
+    }
+
+    htmlElementsForApproval(div, app) {
+        const approveButton = app.generateApprovalButton('approve', "Haha", Joke, this.external_api_id, this.additional_attributes);
+        const rejectButton = app.generateApprovalButton('reject', "Boo! You stink!", Joke, this.external_api_id, this.additional_attributes);
+        const br = document.createElement('br');
+        const spaces = App.createSpaces(1);
+
+        return [div, br, approveButton, spaces[0], rejectButton];
+    }
+
+    static allApprovedFromJson(json) {
+        const count = json.data.length;
+        if (count === 0) {
+            return "No approved jokes";
+        } else {
+            return json.data.map(jokeData => {
+                const {external_api_id, category, setup, punchline} = Joke.getAttributesFromJokeData(jokeData);
+                return new Cat(external_api_id, category, setup, punchline);
+            })
+        }
+    }
+
+    static fromJson(json) {
+        const {external_api_id, category, setup, punchline} = Joke.getAttributesFromJokeData(json);
+        return new Joke(external_api_id, category, setup, punchline);
+    }
+
+    static getAttributesFromJokeData(jokeData) {
+        return {external_api_id: jokeData.id, category: jokeData.type, setup: jokeData.setup, punchline: jokeData.punchline};
+    }
+}
