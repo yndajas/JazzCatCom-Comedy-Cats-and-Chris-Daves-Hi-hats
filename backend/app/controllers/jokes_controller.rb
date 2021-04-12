8595c499a216d3a427d3ad95cfc76944a9ef088f
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-04-13 00:15:23 +0100

    handle additional attributes in jokes#create action

--- backend/app/controllers/jokes_controller.rb
+++ backend/app/controllers/jokes_controller.rb
@@ -6,6 +6,6 @@ class JokesController < ApplicationController
 
     # actually creates a UserJoke record
     def create
-        UserJoke.approve_or_reject(params[:id], params[:identifier], params[:approveOrReject])
+        UserJoke.approve_or_reject(params[:id], params[:identifier], params[:approveOrReject], params[:additional_attributes])
     end
 end
