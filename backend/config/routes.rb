c43d00055cbf41ea8b41c4f46648cc6982e6e4ce
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-04-12 17:45:38 +0100

    add cat routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   post 'sessions' => 'sessions#create'
   delete 'sessions' => 'sessions#destroy'
+
   post 'users' => 'users#create'
-  # resources :users
+
   get 'jazz-videos' => 'jazz_videos#index'
   get 'users/:id/jazz-videos' => 'jazz_videos#index'
   get 'users/:id/jazz-videos/unseen' => 'jazz_videos#index'
   post 'users/:id/jazz-videos' => 'jazz_videos#create'
+
+  get 'users/:id/cats' => 'cats#index'
+  get 'users/:id/cats/random' => 'cats#index'
+  post 'users/:id/cats' => 'cats#create'
 end
