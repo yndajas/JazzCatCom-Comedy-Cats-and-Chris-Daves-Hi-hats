482cf9ca5bf60d03c31f49ed555c01d893d9599d
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-04-12 17:45:08 +0100

    extend Gettable; remove model-specific definition

--- backend/app/models/jazz_video.rb
+++ backend/app/models/jazz_video.rb
@@ -1,19 +1,7 @@
 class JazzVideo < ApplicationRecord
+    extend Gettable::ClassMethods
+
     belongs_to :artist
     has_many :user_jazz_videos
     has_many :users, through: :user_jazz_videos
-
-    def self.get(user_id, request_path)
-        if user_id
-            user = User.find(user_id)
-            if request_path.gsub(/.*\//,"") == "unseen"
-                user.unseen_jazz_videos
-            else
-                user.approved_jazz_videos
-            end
-        else
-            self.all
-        end
-    end
-
 end
