071760a46b0b3c1f6bb7bef56dfad54a6bad2a14
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-04-13 20:27:37 +0100

    generate token when creating user; remove sessions code

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -3,9 +3,8 @@ class UsersController < ApplicationController
         if User.find_by(email: params[:email])
             render json: {error: "User already exists", status: 401}
         else
-            user = User.create(email: params[:email], password: params[:password])
-            session[:user_id] = user.id
-            render json: {user: {id: user.id, email: user.email}}
+            user = User.create(email: params[:email], password: params[:password], token: SecureRandom.urlsafe_base64)
+            render json: {user: {id: user.id, email: user.email, token: user.token}}
         end        
     end
 end
