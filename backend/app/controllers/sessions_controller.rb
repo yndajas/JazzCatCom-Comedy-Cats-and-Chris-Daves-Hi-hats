1c83bb4e363504c4e8e405d03e7e2fb6832be63e
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-04-13 20:26:56 +0100

    update sessions controller to use tokens, not Rails sessions

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -2,14 +2,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user.try(:authenticate, params[:password])
-        session[:user_id] = user.id
-        render json: {user: {id: user.id, email: user.email}}
+        render json: {user: {id: user.id, email: user.email, token: user.token}}
     else
       render json: {error: "Incorrect email and/or password"}, status: 401
     end
-  end
-
-  def destroy
-    session.delete :user_id
   end
 end
