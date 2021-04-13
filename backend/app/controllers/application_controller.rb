9c3f4f1cd378ef63968666f31257bf9648b1367d
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-04-13 20:28:07 +0100

    remove sessions code; update authorisation check

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,19 +1,10 @@
 class ApplicationController < ActionController::API
-    # enable sessions
-    include ActionController::Cookies
-    include ActionController::RequestForgeryProtection
-  
-    def logged_in?
-        !!session[:user_id]
-    end
-
-    def current_user
-        @current_user ||= User.find(session[:user_id])
-    end
-
     def render_error_if_user_mismatch
-        if params[:id] && params[:id] != current_user.id
-            render json: {error: "permission denied"}
+        if params[:id]
+            user = User.find(params[:id])
+            if !user || !(request.headers['token'].try('==', user.token))
+                render json: {error: "access denied"}
+            end
         end
     end
 end
